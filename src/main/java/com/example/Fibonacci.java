8057a9e3f6c552d8339bd842a9c8307dba5f8733
Bayakan <spring.boot@hotmail.com>
2024-05-26 13:00:42 +0300

    Алгоритмы и Структуры Данных. Урок 2: Мемоизация.

--- src/main/java/com/example/Fibonacci.java
+++ src/main/java/com/example/Fibonacci.java
@@ -1,20 +1,35 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
+        int n = 100;
+        long[] mem = new long[n + 1];
+
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibNaive(n, mem));
+      /*  System.out.println(fibNaive(n));
         System.out.println(fibNaive(3));
         System.out.println(fibNaive(5));
-        System.out.println(fibNaive(10));
-        System.out.println(fibEffective(10));
-        System.out.println(fibEffective(100));
+        System.out.println(fibNaive(10));*/
+ /*       System.out.println(fibEffective(10));
+        System.out.println(fibEffective(100));*/
 
     }
 
-    private static long fibNaive(int n) {
+    private static long fibNaive(int n, long[] mem) {
+        if (mem[n] != -1) {
+            return mem[n];
+        }
         if (n <= 1) {
             return n;
         }
-        return fibNaive(n - 1) + fibNaive(n - 2);
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+        mem[n] = result;
+
+        return result;
     }
 
     private static long fibEffective(int n) {
